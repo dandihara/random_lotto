3017bbe6638994c84576bb305f42cefd3e59618e
dandihara <gkaqhd0000@naver.com>
2022-05-11 23:58:17 +0900

    빈도수 체킹

--- recommand_numbers.py
+++ recommand_numbers.py
@@ -6,5 +6,7 @@ conn,cursor = db.connect()
 sql = 'select * from lotto_numbers'
 cursor.execute(sql)
 data = cursor.fetchall()
+time_numbers = [[0 for i in range(45)] for j in range(45)]
 for r in data:
     print(r)
+    
