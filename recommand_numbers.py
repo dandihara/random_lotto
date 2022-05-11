f9fac0661a014b888c02c8e49de96f49fbcc55ac
dandihara <gkaqhd0000@naver.com>
2022-05-12 00:06:13 +0900

    빈도 수 로직 추가

--- recommand_numbers.py
+++ recommand_numbers.py
@@ -7,6 +7,14 @@ sql = 'select * from lotto_numbers'
 cursor.execute(sql)
 data = cursor.fetchall()
 time_numbers = [[0 for i in range(45)] for j in range(45)]
+print(time_numbers)
 for r in data:
-    print(r)
+    numbers = r[2:]
+    for cursor in range(6):
+        for num in range(6):
+            if cursor != num:
+                time_numbers[numbers[cursor]-1][numbers[num]-1] += 1
+
+print(time_numbers)
+        
     
