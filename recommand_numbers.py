933c528611f93f38121a489a876015f978aa9f93
dandihara <gkaqhd0000@naver.com>
2022-05-14 00:00:04 +0900

    빈도수 이용 추천 로직 제작

--- recommand_numbers.py
+++ recommand_numbers.py
@@ -7,7 +7,7 @@ sql = 'select * from lotto_numbers'
 cursor.execute(sql)
 data = cursor.fetchall()
 time_numbers = [[0 for i in range(45)] for j in range(45)]
-print(time_numbers)
+
 for r in data:
     numbers = r[2:]
     for cursor in range(6):
@@ -15,6 +15,19 @@ for r in data:
             if cursor != num:
                 time_numbers[numbers[cursor]-1][numbers[num]-1] += 1
 
-print(time_numbers)
-        
+
+percent_dicts = []
+# 빈도수를 이용한 연관성 
+for i,count_list in enumerate(time_numbers):
+    whole_count = sum(count_list) - count_list[i]
+    count_dict = {}
+    print(whole_count)
+    for j in range(len(count_list)):
+        if j != i:
+            count_dict[i] = round(count_list[j] / whole_count,3)
+        else:
+            count_dict[i] = 0
+    percent_dicts.append(count_dict)
+
+print(percent_dicts)        
     
